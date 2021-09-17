6f60294fb2d85032f6d20a783684e9f37040ed25
Vlad Denisov <vlad.a.denisov@gmail.com>
2021-09-17 17:58:15 +0300

    first attempt findSumOfTwo

--- src/lesson5/task1/Map.kt
+++ src/lesson5/task1/Map.kt
@@ -277,7 +277,15 @@ fun propagateHandshakes(friends: Map<String, Set<String>>): Map<String, Set<Stri
  *   findSumOfTwo(listOf(1, 2, 3), 4) -> Pair(0, 2)
  *   findSumOfTwo(listOf(1, 2, 3), 6) -> Pair(-1, -1)
  */
-fun findSumOfTwo(list: List<Int>, number: Int): Pair<Int, Int> = TODO()
+fun findSumOfTwo(list: List<Int>, number: Int): Pair<Int, Int> {
+    for (x in list.indices) {
+        val t = number - list[x]
+        for (y in x + 1 until list.size) {
+            if (list[y] == t) return Pair(x, y)
+        }
+    }
+    return Pair(-1, -1)
+}
 
 /**
  * Очень сложная (8 баллов)
