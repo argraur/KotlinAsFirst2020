768f94bcea572af5dad1cef65687f23208dede8b
Arseniy Graur <argraur@gmail.com>
2021-10-27 12:02:46 +0300

    lesson7: remove try-catch

--- src/lesson7/task1/Files.kt
+++ src/lesson7/task1/Files.kt
@@ -289,33 +289,29 @@ fun markdownToHtmlSimple(inputName: String, outputName: String) {
     val lines = file.readLines()
     var ongoingParagraph = false
 
-    try {
-        builder.append("<html>")
-        builder.append("<body>")
+    builder.append("<html>")
+    builder.append("<body>")
 
-        lines.forEach {
-            if (!ongoingParagraph) {
-                builder.append("<p>")
-                ongoingParagraph = true
-            } else if (it == "") {
-                builder.append("</p>")
-                ongoingParagraph = false
-            }
-
-            if (ongoingParagraph) {
-                builder.append(md(it))
-            }
+    lines.forEach {
+        if (!ongoingParagraph) {
+            builder.append("<p>")
+            ongoingParagraph = true
+        } else if (it == "") {
+            builder.append("</p>")
+            ongoingParagraph = false
         }
 
-        builder.append("</p>")
-
-        builder.append("</body>")
-        builder.append("</html>")
-
-        File(outputName).writeText(builder.toString())
-    } catch (e: StringIndexOutOfBoundsException) {
-        throw Exception(file.readText())
+        if (ongoingParagraph) {
+            builder.append(md(it))
+        }
     }
+
+    builder.append("</p>")
+
+    builder.append("</body>")
+    builder.append("</html>")
+
+    File(outputName).writeText(builder.toString())
 }
 
 fun md(s: String): String {
